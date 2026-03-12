9096a86c39930a9cacf6597fdf472f4e6074066c
MarcusHiller <marcus.hiller.msg@gmail.com>
2026-03-12 16:55:32 +0100

    vite config expanded and types/node installed, test link added to index

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,16 @@
-import { defineConfig } from 'vite';
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  base: "/dist/"
-});
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        settings: resolve(__dirname, 'setting.html'),
+      },
+    },
+  },
+})
